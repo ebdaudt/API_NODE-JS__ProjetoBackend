4d23687183181b435849656949e0950f52b5604c
Eduardo <ebdaudt@gmail.com>
2023-10-09 08:37:38 -0300

    rotas do projeto

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
+app.use('/api', router);
 
 app.set('port', process.env.PORT || 3333);
 
 module.exports = app;
+
+const router = require('./routes/rotas');
